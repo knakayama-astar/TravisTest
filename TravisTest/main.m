653dd2068b5aa5a1afb29d71eaa85a9a3298a477
中山健志 <ds.rider.kenc1122@gmail.com>
2014-05-16 15:52:18 +0900

    Initial Commit

---
+++ TravisTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TravisTest
+//
+//  Created by 中山健志 on 2014/05/16.
+//  Copyright (c) 2014年 A-STAR. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ TravisTestTests/TravisTestTests.m
@@ -0,0 +1,34 @@
+//
+//  TravisTestTests.m
+//  TravisTestTests
+//
+//  Created by 中山健志 on 2014/05/16.
+//  Copyright (c) 2014年 A-STAR. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TravisTestTests : XCTestCase
+
+@end
+
+@implementation TravisTestTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
