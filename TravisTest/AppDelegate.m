00563f15661df8711d3654782f10cc092a4ce23c
中山健志 <ds.rider.kenc1122@gmail.com>
2014-05-16 16:27:13 +0900

    わざとビルドエラー

--- TravisTest/AppDelegate.m
+++ TravisTest/AppDelegate.m
@@ -20,6 +20,8 @@
     // Override point for customization after application launch.
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
+    
+    aa
     return YES;
 }
 
